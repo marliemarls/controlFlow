386b6983add7810dc4fab5668d3200f47b1156e8
Marlie Martinez <marlieahue@gmail.com>
2024-05-09 21:57:54 -0400

    so far what i got going on

--- script.js
+++ script.js
@@ -1,23 +1,31 @@
 // using the given information I can create two variables that are immutable and underneath the declared variables I will create a function 
 const PI = 3.1415;
 const radius = 5;
-// const area = PI * radius * radius;
+const area = PI * radius * radius;
 // i calculated the area in meters of the space with the equation and info that was given and decided to make the variable area a number instead of the equation 
-const area = 78.54
 // created a variable for minimum space for plants 
 const minimumSpace = 0.8
 let plants = 20;
 
-//to determine the area that the plants need 
-let week1 = plants * minimumSpace
-let week2 = (week1 * 2);
-let week3 = (week2 * 2);
+//to determine the area that the plants will have in the weeks 1-3
+let week1 = plants * minimumSpace /* 16*/
+let week2 = (week1 * 2); /* 32*/
+let week3 = (week2 * 2); /* 64*/
 
-console.log()
+console.log(week1, week2, week3)
 
-let week1space = (week1 / area) * 100;
-let week2space = (week2 / area) * 100;
-let week3space = (week3 / area) * 100;
+//percentage of area that the plants are taking up 
+let week1space = (week1 / area) * 100; /* 20*/
+let week2space = (week2 / area) * 100; /* 40*/
+let week3space = (week3 / area) * 100; /* 80*/
+
+console.log(week1space, week2space, week3space)
+//
+let weekArray = [week1space, week2space, week3space]
+switch(weekArray){
+    case [0]: 
+            if([0], )
+}
 
 
 
