c2d178f005b1c8049ba1bb67b6cafcd7d4a4868e
Marlie Martinez <marlieahue@gmail.com>
2024-05-13 20:52:15 -0400

    uhhh

--- script.js
+++ script.js
@@ -29,10 +29,9 @@ console.log(week1space, week2space, week3space)
 // }
 
 function plantsGrowth(weeks){
-    const plantsGrowing = plants * (2 ** weeks)
     if(weeks === 1){
         console.log("Theres more room for plants to be planted! The percentage of area the plants are taking up is " + week1space + ".")
-    } if() {
+    } if(weeks === 2) {
         console.log("They are growing at an acceptable rate, but the plants should be monitored. " + `${week2space}% of the area is being used.` )
     } if(weeks >= 3){
         console.log("The plants must be pruned." + `${week3space}% of the area is being taken up.`)
